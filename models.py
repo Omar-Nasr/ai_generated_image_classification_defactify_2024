ad621c623ede2ca0c5a55c40d08a3e04abd7a7b8
Omar Nasr <omarnasrempires204@gmail.com>
2024-11-11 15:26:27 +0200

    Testing Validation Handling

--- models.py
+++ models.py
@@ -3,12 +3,12 @@ from data_handler import Image_Classification_Dataset
 from torch.utils.data import DataLoader
 from torchvision import models
 import torch
-def train_swin(train_data_dir,checkpoint_path,num_epochs=10,batch_sz=16,task="binary"):
+def train_swin(train_data_dir,checkpoint_path,num_epochs=10,val_data_dir=None,val_labels=None,val=False,batch_sz=16,task="binary"):
     model = models.swin_v2_b(pretrained=True)
     from data_handler import Image_Classification_Dataset
     from torch.utils.data import DataLoader
-    train_data_dir = "/kaggle/input/d/omarnasr/ai-image-classification-2/" 
     img_dataset = Image_Classification_Dataset(train_data_dir)
+
     train_dataloader = DataLoader(img_dataset,batch_sz,num_workers=4)
     optimizer = torch.optim.Adam(params=model.parameters(),lr=1e-5)
     scheduler = torch.optim.lr_scheduler.ConstantLR(optimizer)
@@ -17,5 +17,10 @@ def train_swin(train_data_dir,checkpoint_path,num_epochs=10,batch_sz=16,task="bi
         classifier = torch.nn.Linear(1000,2)
     else:
         classifier = torch.nn.Linear(1000,6)
-    model_trained = train_model(model,criterion,optimizer,scheduler,train_dataloader,classifier,num_epochs,checkpoint_path,task)
+    if(val==True):
+        val_dataset = Image_Classification_Dataset(val_data_dir,task="Binary",val=True,val_labels=val_labels)
+        val_dataloader = DataLoader(val_dataset,batch_sz,num_workers=4)
+        model_trained = train_model(model,criterion,optimizer,scheduler,train_dataloader,classifier,num_epochs,checkpoint_path,task,use_fourrier=True,model_name="swin",val_dataloader=val_dataloader)
+    else:
+        model_trained = train_model(model,criterion,optimizer,scheduler,train_dataloader,classifier,num_epochs,checkpoint_path,task,use_fourrier=True,model_name="swin")
     return model_trained
