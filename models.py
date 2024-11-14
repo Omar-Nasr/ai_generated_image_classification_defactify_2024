0a07a80334cbe833ea08e54d8160236122c73358
Omar Nasr <omarnasrempires204@gmail.com>
2024-11-14 16:49:05 +0200

    Slight changes

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader
 from torchvision import models
 import torch
 from adopt import ADOPT
-def train_classifier(train_data_dir,checkpoint_path,num_epochs=10,val_data_dir=None,val_labels=None,val=False,batch_sz=16,task="binary",model_name="swin",optimizer_name="adam"):
+def train_classifier(train_data_dir,checkpoint_path,num_epochs=10,val_data_dir=None,val_labels=None,val=False,batch_sz=16,task="binary",model_name="swin",optimizer_name="adam",use_fourier=False):
     if(model_name=="swin"):
         model = models.swin_v2_b(pretrained=True)
     elif(model_name=="vit"):
@@ -31,7 +31,7 @@ def train_classifier(train_data_dir,checkpoint_path,num_epochs=10,val_data_dir=N
     if(val==True):
         val_dataset = Image_Classification_Dataset(val_data_dir,task=task,val=True,val_labels=val_labels)
         val_dataloader = DataLoader(val_dataset,batch_sz,num_workers=4)
-        model_trained = train_model(model,criterion,optimizer,scheduler,train_dataloader,classifier,num_epochs,checkpoint_path,task,use_fourrier=False,model_name="swin",val_dataloader=val_dataloader)
+        model_trained = train_model(model,criterion,optimizer,scheduler,train_dataloader,classifier,num_epochs,checkpoint_path,task,use_fourrier=use_fourier,model_name=model_name,val_dataloader=val_dataloader)
     else:
-        model_trained = train_model(model,criterion,optimizer,scheduler,train_dataloader,classifier,num_epochs,checkpoint_path,task,use_fourrier=False,model_name="swin")
+        model_trained = train_model(model,criterion,optimizer,scheduler,train_dataloader,classifier,num_epochs,checkpoint_path,task,use_fourrier=use_fourier,model_name=model_name)
     return model_trained
