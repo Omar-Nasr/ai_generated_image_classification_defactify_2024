210e6951af30812002eb24094d4a2a082a8ec31d
Omar Nasr <omarnasrempires204@gmail.com>
2024-12-05 03:42:15 +0200

    freezing model

--- models.py
+++ models.py
@@ -45,6 +45,7 @@ def train_classifier(train_data_dir,checkpoint_path,num_epochs=10,val_data_dir=N
         val_dataset = Image_Classification_Dataset(val_data_dir,task=task,val=True,val_labels=val_labels)
         val_dataloader = DataLoader(val_dataset,batch_sz,num_workers=4)
         if(classical_ml==True):
+            model.eval()
             model_trained=train_classical_classifier(model,train_dataloader,val_dataloader,batch_sz,num_epochs)
         else:
             model_trained = train_model(model,criterion,optimizer,optimizer2,scheduler,scheduler2,train_dataloader,classifier,num_epochs,checkpoint_path,task,use_fourier=use_fourier,model_name=model_name,val_dataloader=val_dataloader,batch_sz=batch_sz,fine_tune=fine_tune,trial=trial)
