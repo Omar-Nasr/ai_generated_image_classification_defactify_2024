b4341a68bc6da59e071704e992069b7c8357e760
Omar Nasr <omarnasrempires204@gmail.com>
2024-12-08 07:11:30 +0200

    bugfix

--- training_loop.py
+++ training_loop.py
@@ -111,6 +111,7 @@ def train_model(model,criterion,optimizer,optimizer2,scheduler,scheduler2,train_
                     best_f1 = curr_f1
                     best_val_f1 = curr_f1
                     torch.save(model.state_dict(),checkpoint_path)
+                    torch.save(classifier.state_dict(),checkpoint_path)
                 f.write(f'Epoch ${epoch} Val Loss: ${running_loss} \n')
                 print(f'Epoch ${epoch} Val Loss: ${running_loss} \n')
 
@@ -125,6 +126,7 @@ def train_model(model,criterion,optimizer,optimizer2,scheduler,scheduler2,train_
         f.write(f'Best F1: {best_f1} after {num_epochs} Epochs')
         print(f'Best F1: {best_f1} after {num_epochs} Epochs')      
         model.load_state_dict(torch.load(checkpoint_path))
+        classifier.load_state_dict(torch.load(classifier_path))
         return model,classifier
 
 
